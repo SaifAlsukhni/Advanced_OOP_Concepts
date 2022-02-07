4d059f5ecdd8094eb621e3e726b9a0da5204eb1d
SaifAlsukhni <70297354+SaifAlsukhni@users.noreply.github.com>
2022-02-06 19:12:07 -0600

    OOP Review Done

--- OOP Review/Inheritance/src/Shape.java
+++ OOP Review/Inheritance/src/Shape.java
@@ -4,8 +4,8 @@ public class Shape {
     public void setName(String name){this.name = name;}
     public double getArea(){return 0.0;}
     public double getPerimeter(){return 0.0;}
-    /* TODO : Implement a constructor that takes in a String as parameter and sets the name of this object.*/
 
+    //constructor
     public Shape(String name) {
         this.name = name;
     }
--- OOP Review/Inheritance/src/Square.java
+++ OOP Review/Inheritance/src/Square.java
@@ -1,8 +1,3 @@
-/* TODO : Complete the Square class.
- Add variables if you need.
- Remember to use the @Override to override methods.
-intelliJ will be helpful if you do).*/
-
 public class Square extends Shape {
 
     public double side;
