35dddd2e243981142facdc07e890fb95d44e5df6
SaifAlsukhni <70297354+SaifAlsukhni@users.noreply.github.com>
2022-03-28 20:09:55 -0500

    Threads And Concurrency Done

--- ThreadsAndConcurrency/RacingCars/src/Racer.java
+++ ThreadsAndConcurrency/RacingCars/src/Racer.java
@@ -1,12 +1,33 @@
-public class Racer /* TODO 
-Add something here so that you can pass this Racer to the constructor of Thread*/{
+public class Racer implements Runnable {
+
     Car c;
     int distance = 1000; // this is a racer that is going to run 1000 miles.
 
     /* TODO 
 Create a constructor that receives a Car objec and initializes c.*/
+    public Racer(Car c) {
+        this.c = c;
+    }
+
     @Override
     /* TODO 
 Override one method.  This is the task the thread will perform... You have to determine which method this is.
 Check the method description. */
+    public void run() {
+        while (this.c.odometer < distance) {
+            if (this.c.miles == 0) {
+                try {
+                    System.out.println("Charging " + this.c.name);
+                    this.c.charge();
+                    Thread.sleep((int) this.c.miles);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (this.c.miles > 0) {
+                this.c.move(1);
+            }
+        }
+    }
+
 }
--- ThreadsAndConcurrency/RacingCars2/src/Racer.java
+++ ThreadsAndConcurrency/RacingCars2/src/Racer.java
@@ -1,3 +1,27 @@
-public class Racer /* TODO 
-This class is exactly as the Racer from Task 1*/
+public class Racer implements Runnable {
+
+    Car c;
+    int distance = 1000; // this is a racer that is going to run 1000 miles.
+
+    public Racer(Car c) {
+        this.c = c;
+    }
+
+    @Override
+    public void run() {
+        while (this.c.odometer < distance) {
+            if (this.c.miles == 0) {
+                try {
+                    System.out.println("Charging " + this.c.name);
+                    this.c.charge();
+                    Thread.sleep((int) this.c.miles);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (this.c.miles > 0) {
+                this.c.move(1);
+            }
+        }
+    }
 }
